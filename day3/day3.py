83b9aa16ce0b831ff1ea5a1b3257663928b9b0f9
Jon Fingas <jon.fingas@gmail.com>
2023-12-03 18:43:16 -0600

    re-working

--- day3/day3.py
+++ day3/day3.py
@@ -1,16 +1,6 @@
 data = open("day3-test.input")
 lines = data.readlines()
-symCoords = []
-numCoords = []
-for line in enumerate(lines, start=1):
-    for char in enumerate(line[1].strip(), start=1):
-        if not char[1].isnumeric() and char[1] != ".":
-            symCoords.append((line[0],char[0]))
-        if char[1].isnumeric():
-            numCoords.append((line[0],char[0]))
-print(f"symCoords: {symCoords}")
-print(f"numCoords: {numCoords}")
-
-for symbol in symCoords:
-    print(f"Symbol: {symbol}")
-    
+#Get dimensions of grid
+#Scan grid for symbol
+#- Once symbol is found, scan all adjacent spots for a number, save their coordinate
+#Scan through columns in coordinates to get whole numbers.
