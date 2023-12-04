f8559de51127bfe2bae2726658f01cb49dc29555
Jon Fingas <jon.fingas@gmail.com>
2023-12-03 22:34:53 -0600

    more work

--- day3/day3.py
+++ day3/day3.py
@@ -3,17 +3,24 @@ lines = data.read().splitlines()
 #Get dimensions of grid
 gridHeight = len(lines)
 gridWidth = len(lines[0])
-print(f"gridHeight: {gridHeight}, gridWidth: {gridWidth}")
 #Scan grid for symbol
 symbolCoords = set()
+partNumbers = []
 for rIndex, row in enumerate(lines):
     for cIndex, column in enumerate(row):
         if not column.isnumeric() and column != ".":
+            #Add symbol to coord set
             symbolCoords.add((rIndex, cIndex))
-#- Once symbol is found, scan all adjacent spots for a number, save their coordinate
-for x, symbol in enumerate(symbolCoords):
-    print(f"row: {lines[symbol[0]][symbol[1]]}")
-    print(f"row: {lines[symbol[0]-x][symbol[1]]}")
 
-#Scan through columns in coordinates to get whole numbers.
-print(f"Symcoords {symbolCoords}")
+#- Once symbol is found, scan all adjacent spots for a number, save their coordinate
+for symbol in symbolCoords:
+    #symbolCoords(Line, Char)
+    for char in lines[symbol[0]]:
+        adj = [0,1,-1]
+        for x in adj:
+            if (symbol[0] - x) >= 0 and (symbol[0] - x) <= len(lines) -1:
+                print(f"char: {lines[symbol[0]-x][symbol[1]]} x:{x} symbol:{symbol}")
+            if (symbol[1] - x) >= 0 and (symbol[1] - x) <= len(lines[symbol[0]]) -1:
+                print(f"char: {lines[symbol[0]-x][symbol[1]]} x:{x} symbol:{symbol}")
+
+    print("---------")
