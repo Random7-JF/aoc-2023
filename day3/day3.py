4cec84b0125f8413cccc564dff6dc01de26a1423
Jon Fingas <jon.fingas@gmail.com>
2023-12-03 21:57:14 -0600

    more work

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,19 @@
-data = open("day3-test.input")
-lines = data.readlines()
+data = open("/home/random/projects/aoc-2023/day3/day3-test.input")
+lines = data.read().splitlines()
 #Get dimensions of grid
+gridHeight = len(lines)
+gridWidth = len(lines[0])
+print(f"gridHeight: {gridHeight}, gridWidth: {gridWidth}")
 #Scan grid for symbol
+symbolCoords = set()
+for rIndex, row in enumerate(lines):
+    for cIndex, column in enumerate(row):
+        if not column.isnumeric() and column != ".":
+            symbolCoords.add((rIndex, cIndex))
 #- Once symbol is found, scan all adjacent spots for a number, save their coordinate
+for x, symbol in enumerate(symbolCoords):
+    print(f"row: {lines[symbol[0]][symbol[1]]}")
+    print(f"row: {lines[symbol[0]-x][symbol[1]]}")
+
 #Scan through columns in coordinates to get whole numbers.
+print(f"Symcoords {symbolCoords}")
